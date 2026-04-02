a6104a0535997856ad1b215471bf0293aaebba74
fabinhokussym <fabinhokussym@gmail.com>
2026-04-02 13:33:08 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'biblioteca-da-lua-v1';
+const CACHE = 'biblioteca-da-lua-v2';
 
 const ASSETS = [
   './',
@@ -6,12 +6,20 @@ const ASSETS = [
   './manifest.json',
   './icons/icon-192.png',
   './icons/icon-512.png',
-  './00.jpg', './01.jpg', './02.jpg', './03.jpg',
-  './04.jpg', './05.jpg', './06.jpg', './07.jpg',
-  './08.jpg', './09.jpg', './10.jpg', './11.jpg'
+  './pascoa-da-lua/00_L.jpg', './pascoa-da-lua/00_R.jpg',
+  './pascoa-da-lua/01_L.jpg', './pascoa-da-lua/01_R.jpg',
+  './pascoa-da-lua/02_L.jpg', './pascoa-da-lua/02_R.jpg',
+  './pascoa-da-lua/03_L.jpg', './pascoa-da-lua/03_R.jpg',
+  './pascoa-da-lua/04_L.jpg', './pascoa-da-lua/04_R.jpg',
+  './pascoa-da-lua/05_L.jpg', './pascoa-da-lua/05_R.jpg',
+  './pascoa-da-lua/06_L.jpg', './pascoa-da-lua/06_R.jpg',
+  './pascoa-da-lua/07_L.jpg', './pascoa-da-lua/07_R.jpg',
+  './pascoa-da-lua/08_L.jpg', './pascoa-da-lua/08_R.jpg',
+  './pascoa-da-lua/09_L.jpg', './pascoa-da-lua/09_R.jpg',
+  './pascoa-da-lua/10_L.jpg', './pascoa-da-lua/10_R.jpg',
+  './pascoa-da-lua/11_L.jpg', './pascoa-da-lua/11_R.jpg',
 ];
 
-// Instala e cacheia tudo
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE).then(cache => cache.addAll(ASSETS))
@@ -19,7 +27,6 @@ self.addEventListener('install', e => {
   self.skipWaiting();
 });
 
-// Ativa e limpa caches antigos
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
@@ -29,7 +36,6 @@ self.addEventListener('activate', e => {
   self.clients.claim();
 });
 
-// Serve do cache primeiro, depois da rede
 self.addEventListener('fetch', e => {
   e.respondWith(
     caches.match(e.request).then(cached => cached || fetch(e.request))
